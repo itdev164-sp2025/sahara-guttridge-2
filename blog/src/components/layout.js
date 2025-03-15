99799d9da51ca0fcbedb9e29437a5d60f8d7ae57
Sahara Guttridge <guttrids@gmatc.matc.edu>
2025-03-15 12:07:44 -0500

    Completed ACTUAL Activity 4

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -7,9 +7,21 @@
 
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import styled, { ThemeProvider } from "styled-components"
 
 import Header from "./header"
-import "./layout.css"
+import "./layout.css" 
+import {Gray} from './themes/gray'
+
+const Content = styled.div`
+margin: 0 auto;
+max-width: var(--size-content);
+padding: var(--size-gutter);
+`
+const Footer = styled.footer`
+margin-top: var(--space-5);
+font-size: var(--font-sm);
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -23,28 +35,20 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
+    
+     <ThemeProvider theme ={Gray} >
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`}/>
+      <Content>
+        <main>
+          {children}
+        </main>
+        <Footer>
+          {new Date().getFullYear()} &middot; Built with{` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+        </Footer>
+      </Content>
+    </ThemeProvider>
+    
   )
 }
 
---
+++ blog/src/components/themes/Gray.js
@@ -0,0 +1,9 @@
+
+const theme={
+    header: {
+        backgroundColor: `var(--color-primary)`,
+        color: `var(--color-contrast)`
+    }
+}
+
+export{theme as Gray}
